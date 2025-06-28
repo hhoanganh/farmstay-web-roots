dbda3ca81ff5b86820acd6c1cd4cf92b5fc0415a
hhoanganh <hhoanganh@users.noreply.github.com>
2025-06-28 12:59:32 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -64,7 +64,7 @@ const Homepage = () => {
                 className={`fade-in-scroll slide-in-left ${homestayAnimation.isVisible ? 'visible' : ''}`}
               >
                 <ImageWrapper 
-                  src="https://bdsquan9.vn/wp-content/uploads/2021/06/%C4%91%E1%BA%A5t-n%E1%BB%81n-h%E1%BB%93-%C4%91%E1%BA%A1-sar-l%C3%A2m-h%C3%A0.jpg" 
+                  src="https://ik.imagekit.io/offvxi40h/our-farmstay-2.jpg?" 
                   alt="Our homestay rooms nestled in the mountains"
                   className="max-w-md mx-auto"
                 />
@@ -107,7 +107,7 @@ const Homepage = () => {
               className={`md:order-2 fade-in-scroll slide-in-right ${farmAnimation.isVisible ? 'visible' : ''}`}
             >
               <ImageWrapper 
-                src="https://ik.imagekit.io/offvxi40h/our-farmstay-2.jpg" 
+                src="https://thoibaonganhang.vn/stores/news_dataimages/minhvl/012022/17/02/1220_d2010mh-1603226412403.jpg" 
                 alt="A tree in the farm garden"
                 className="max-w-md mx-auto transform rotate-2"
               />
