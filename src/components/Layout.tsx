80425725410357767d1c60de79fd118739ce1362
hhoanganh <hhoanganh@users.noreply.github.com>
2025-06-28 16:36:32 +0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 const Layout = () => {
 
 
   return (
     <div className="min-h-screen flex flex-col bg-[hsl(var(--background-primary))]">
+      <ScrollToTop />
       <Header />
       {/* pt-20 provides top padding to prevent content from being obscured by the fixed Header */}
       <main className="flex-1 pt-20">
